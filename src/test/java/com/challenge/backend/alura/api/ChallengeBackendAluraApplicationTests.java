ffbc6811a51621cc3266160dd4ac45ccac1795c6
Fernando Guedes <fernando.guedes@cwi.com.br>
2023-07-25 08:46:36 -0300

    :tada: Commit Inicial

---
+++ src/test/java/com/challenge/backend/alura/api/ChallengeBackendAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.backend.alura.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBackendAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
